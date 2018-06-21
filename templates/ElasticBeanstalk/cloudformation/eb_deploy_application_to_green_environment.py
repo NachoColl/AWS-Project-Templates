398e8227c240cf5daf5a282d9da016917ca628fb
Nacho Coll <ignacio.coll@gmail.com>
2018-06-21 18:24:13 +0200

    minors

--- templates/ElasticBeanstalk/cloudformation/eb_deploy_application_to_green_environment.py
+++ templates/ElasticBeanstalk/cloudformation/eb_deploy_application_to_green_environment.py
@@ -5,40 +5,39 @@ import sys
 import logging
 import threading
 import time
+import io
+import gzip
+import shutil
 
 from time import sleep
 from datetime import datetime, timezone
 from botocore.config import Config
 
 beanstalkclient = boto3.client('elasticbeanstalk')
-codepipelineclient = boto3.client('codepipeline')
 lambdaclient = boto3.client('lambda')
 
-Message = ""
-Status  = ""
+def lambda_handler_test_raise_error(event, context):
+    print(event)
+    raise Exception('I failed!')
+    return { "EnvironmentId":"pepe" }
 
 def lambda_handler(event, context):
-    global Status
-    global Message
     
+    # event must contain:
+    # { "BeanstalkApplicationName":"deployment-tests", "BeanstalkApplicationConfigurationTemplate":"AMS-D-Elast-JXD24ZW3JQ47", "S3Bucket":"mybucket", "S3Key":"/mypath/myfil.zip"}
+    print(event)
+    
+    # to catch lambda timeout
     timer = threading.Timer((context.get_remaining_time_in_millis() / 1000.00) - 0.5, timeout, args=[event, context])
     timer.start()
     
     try:
         
-        # Extract the Job ID
-        job_id = event['CodePipeline.job']['id']
-       
-        # Extract the Job Data
-        job_data = event['CodePipeline.job']['data']
-        # The Artifacts
-        input_artifact  = job_data['inputArtifacts'][0]
-        output_artifact = job_data['outputArtifacts'][0]
-        # parameters
-        user_parameters = job_data['actionConfiguration']['configuration']['UserParameters']
-        # { "BeanstalkApplicationName":"deployment-tests", "BeanstalkApplicationConfigurationTemplate":"AMS-D-Elast-JXD24ZW3JQ47"}
-        application_name = (json.loads(user_parameters)['BeanstalkApplicationName'])
-        configuration_template = (json.loads(user_parameters)['BeanstalkApplicationConfigurationTemplate'])     
+        application_name = event['BeanstalkApplicationName']
+        configuration_template = event['BeanstalkApplicationConfigurationTemplate']  
+        s3_artifact_bucket = event['S3Bucket'] 
+        s3_artifact_key = event['S3Key'] 
+        
         # unique id for assets naming
         unique_id = datetime.now().isoformat('-').replace('.','-').replace(':','-')
         
@@ -47,47 +46,46 @@ def lambda_handler(event, context):
             # get info about any existing blue environment (e.g. to get the name)
             blue_environment = get_blue_environment(application_name)
             print(blue_environment)
+            
             # create a new application version
-            application_version = create_application_version(application_name, unique_id, job_id, input_artifact)
+            application_version = create_application_version(application_name, unique_id, s3_artifact_bucket, s3_artifact_key)
             print(application_version)
             
             # create a green environment for the related app version
             green_environment = create_environment(application_name, application_version, configuration_template, blue_environment, unique_id)
             print(green_environment)
             
-            # write outputs
             try:
-                print(green_environment['EnvironmentId'])
-                write_output(green_environment['EnvironmentId'], event, output_artifact)
-                Message = "New environment created."
-                Status  = "Success"
+                if (blue_environment is None):
+                    return { "EnvironmentId": green_environment['EnvironmentId']}
+                else:
+                    return { "EnvironmentId": green_environment['EnvironmentId'], "BlueEnvironmentId": blue_environment['EnvironmentId']}
             except Exception as e:
+                
                 print("Invoke 'arch_codepipeline_eb_terminate_environment' to delete created environment")
                 terminate_environment_payload = {
                     'EnvironmentId':green_environment['EnvironmentId'],
                     'ApplicationName':application_name
                 }
+                
                 response = lambdaclient.invoke(
                     FunctionName='arch_codepipeline_eb_terminate_environment',
                     InvocationType='Event',
                     Payload=json.dumps(terminate_environment_payload)
                 )
-                catch_exception(e)
-                
+                raise Exception('unexpected error (check the lambda logs)')
+
         else:
-            Message = "Environment change in progress."
-            Status  = "Failure"
+            print('Environment change in progress, cannot create a new one.')
+            raise Exception('unexpected error (check the lambda logs)')
             
     except Exception as e:
         catch_exception(e)
+        raise Exception('unexpected error (check the lambda logs)')
 
     finally:
-
         timer.cancel()
-        if (Status =="Success"):
-            put_job_success(job_id)
-        else:
-            put_job_failure(job_id)
+
 
 def catch_exception(e):
     global Status
@@ -97,9 +95,6 @@ def catch_exception(e):
     e = sys.exc_info()[0]
     print(e)
     traceback.print_exc()
-    
-    Status="Failure"
-    Message=("Error occured while executing this. The error is %s" %e)
 
     return
 
@@ -118,18 +113,15 @@ def environment_change_in_progress(ApplicationName):
     return False  
 
 # creates a new application version
-def create_application_version(ApplicationName, UniqueId, JobId, Artifact):
-    
-    artifact_key = Artifact['location']['s3Location']['objectKey']
-    artifact_bucket = Artifact['location']['s3Location']['bucketName']
-    
+def create_application_version(ApplicationName, UniqueId, S3Bucket, S3Key):
+
     response = beanstalkclient.create_application_version(
         ApplicationName=ApplicationName,
         VersionLabel=UniqueId,
-        Description='created from codepipeline job id: ' + JobId,
+        Description='created from deployment codepipeline',
         SourceBundle={
-            'S3Bucket': artifact_bucket,
-            'S3Key': artifact_key
+            'S3Bucket': S3Bucket,
+            'S3Key': S3Key
         },
         AutoCreateApplication=False,
         Process=True
@@ -206,7 +198,7 @@ def create_environment(ApplicationName, ApplicationVersion, ConfigTemplate, Blue
     return response
 
 # writes endpoint url to output artifact (1)
-def write_output(EnvironmentId, Event, Artifact):
+def write_output(EnvironmentId, Artifact):
     
     artifact_key = Artifact['location']['s3Location']['objectKey']
     artifact_bucket = Artifact['location']['s3Location']['bucketName']
@@ -214,11 +206,20 @@ def write_output(EnvironmentId, Event, Artifact):
     config = Config(signature_version='s3v4')
     s3client = boto3.client("s3", config=config)
     
-    response = s3client.put_object(
-        Body=EnvironmentId, 
-        Bucket=artifact_bucket, 
-        Key=artifact_key, 
-        ServerSideEncryption='aws:kms')
+    # A GzipFile must wrap a real file or a file-like object. We do not want to
+    # write to disk, so we use a BytesIO as a buffer.
+    gz_body = io.BytesIO()
+    gz = gzip.GzipFile(None, 'wb', 9, gz_body)
+    gz.write(EnvironmentId.encode('utf-8'))  # convert unicode strings to bytes!
+    gz.close()
+    
+    response =  s3client.put_object(
+        Bucket=artifact_bucket,
+        Key=artifact_key,  # Note: NO .gz extension!
+        ContentType='text/plain',  # the original type
+        ContentEncoding='gzip',  # MUST have or browsers will error
+        Body=gz_body.getvalue()
+    )
     
     return response
 
@@ -233,17 +234,4 @@ def first_word(input, character):
 def timeout(event, context):
 
     logging.error('Execution is about to time out, sending failure response to CodePipeline')
-    put_job_failure(event['CodePipeline.job']['id'], "FunctionTimeOut")
-
-
-def put_job_success(job):
-
-    print('Putting job success')
-    print(Message)
-    codepipelineclient.put_job_success_result(jobId=job)
-
-def put_job_failure(job):
-
-    print('Putting job failure')
-    print(Message)
-    codepipelineclient.put_job_failure_result(jobId=job, failureDetails={'message': Message, 'type': 'JobFailed'})
+    raise Exception('unexpected error (check the lambda logs)')
